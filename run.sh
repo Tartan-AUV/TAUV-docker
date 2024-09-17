4937cd54603068b1b708d174f69ec397728b2f94
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2024-09-17 18:43:54 -0400

    wip

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rocker --nvidia --x11 --privileged --home --port 22$(id -u | rev | cut -c1-3 | rev):22 tauv/x86-nvidia-workstation
+rocker --nvidia --x11 --privileged --home --port 22$(id -u | rev | cut -c1-3 | rev):22 tauv/x86-nvidia-workstation > /dev/null 2>&1 &
+
+echo "Docker container started, ssh port 22$(id -u | rev | cut -c1-3 | rev)."
