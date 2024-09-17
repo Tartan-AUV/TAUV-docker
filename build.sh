fc848d31d4a087c5c8d1893446f6fd90e0f58275
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2024-09-17 18:07:30 -0400

    wip

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ fi
 
 docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --build-arg USER_NAME="$USER" --build-arg USER_PASSWORD="$PASSWORD" --file apps/Dockerfile.workstation --tag tauv/x86-nvidia-workstation .
 
-echo -e "\n\n\nFinished building the workstation image.\nYour SSH port is 22$(id -u | rev | cut -c1-3 | re)."
+echo -e "\n\n\nFinished building the workstation image.\nYour SSH port is 22$(id -u | rev | cut -c1-3 | rev)."
 
