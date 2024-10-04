c8e511346fc88e96940a0aa2cada23b445cca0f4
Gleb Ryabtsev <89747634+GlebRyabtsev@users.noreply.github.com>
2024-10-04 16:20:12 -0400

    Working VNC with VGL (#4)
    TurboVNC with LXQT desktop environment, novnc front-end, and working VGL rendering with EGL back-end.

--- build.sh
+++ build.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-docker build --file platform/Dockerfile.x86-nvidia --tag tauv/x86-nvidia-platform .
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-platform --file common/Dockerfile.common --tag tauv/x86-nvidia-common .
+docker build --file platform/Dockerfile.x86-nvidia-turbovnc --tag tauv/x86-nvidia-turbovnc-platform .
+docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-turbovnc-platform --file common/Dockerfile.common --tag tauv/x86-nvidia-common .
 
-read -sp "Enter the password for the Docker user: " PASSWORD
-echo
-read -sp "Confirm the password: " PASSWORD_CONFIRM
-echo
-if [ "$PASSWORD" != "$PASSWORD_CONFIRM" ]; then
-    echo "Passwords do not match. Please try again."
-    exit 1
-fi
+docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --build-arg USER_NAME="$USER" --build-arg USER_PASSWORD="$PASSWORD" --file apps/Dockerfile.workstation --tag tauv/x86-nvidia-workstation .
 
-echo "Using user name $USER"
-
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --build-arg SSH_USER_NAME="$USER" --build-arg SSH_USER_PASSWORD="$PASSWORD" --file apps/Dockerfile.workstation --tag tauv/x86-nvidia-workstation .
-
-echo -e "\n\n\nFinished building the workstation image.\nYour SSH port is 22$(id -u | rev | cut -c1-3 | rev)."
+echo -e "\n\n\nFinished building the workstation image."
 
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Check if there are any running Docker containers
+if [ "$(docker ps -q)" ]; then
+    echo "There are running container. You can see them by running 'docker ps' and stop them by calling 'docker kill <container name>'. Aborting..."
+    exit 1
+fi
+
 if [[ -z "${DOCKER_SHARED_DIR}" ]]; then
     mkdir -p $HOME/shared
     SHARED_DIR=$HOME/shared
@@ -9,8 +17,8 @@ fi
 
 echo "Using shared folder: $SHARED_DIR"
 
-echo "Starting container, SSH port 22$(id -u | rev | cut -c1-3 | rev)."
+echo "Starting container, VNC address: http://tauv-dev.lan.local.cmu.edu:60$(id -u | rev | cut -c1-3 | rev)/vnc.html"
 
-rocker --nvidia --x11 --privileged --port 22$(id -u | rev | cut -c1-3 | rev):22 --volume $SHARED_DIR:$SHARED_DIR -- tauv/x86-nvidia-workstation
+rocker --nvidia --privileged --port 60$(id -u | rev | cut -c1-3 | rev):8080 --volume $SHARED_DIR:/shared --volume /dev/shm:/dev/shm -- tauv/x86-nvidia-workstation
 
 
